4f9be8bebbb4f0a891184ff36af99e3eefe76ca1
Yuan <41229059+gy-0@users.noreply.github.com>
2024-02-13 01:38:34 -0500

    Create getCardBrandById.js

---
+++ getCardBrandById.js
@@ -0,0 +1,88 @@
+{
+  "rspMsgCd" : "MCA00000",
+  "data" : {
+    "cards" : [
+      {
+        "desc" : {
+          "language" : "zh",
+          "additionalDesc" : "线上充值最高96折，可反复续充，无过期日||此会员储值卡仅能在Hamilton Nations超市门店内使用||充值折扣与店内优惠是否可以叠加使用以商户规则为准||此卡中金额不够当次消费，可扣清卡金额后用其他方式补齐（刷银行卡，付现等）",
+          "name" : "Hamilton Nations购物卡",
+          "description" : "此卡为记名制，仅限本人使用不可转借他人||此卡不可做转账或记账使用||开卡与充值一经办理，非店方原因，不兑现，不退换||此卡最终解释权归商户所有||Snaplii是第三方购物卡购买平台，此购物卡发行商为Nations超市并负责资金管理"
+        },
+        "bestDeal" : "false",
+        "cardTemplateId" : "0000007327",
+        "status" : "ACTIVE",
+        "imageUrl" : "https://snaplii.s3-us-west-2.amazonaws.com/snaplii/card/1699304835016_nations.png",
+        "discount" : "2",
+        "balanceRefreshable" : "true",
+        "reloadable" : "true",
+        "type" : "PREPAID_CARD",
+        "buyAmount" : "49",
+        "faceValueRules" : {
+          "type" : "FIXED",
+          "priceStart" : "50",
+          "priceEnd" : "50"
+        }
+      },
+      {
+        "desc" : {
+          "language" : "zh",
+          "additionalDesc" : "线上充值最高96折，可反复续充，无过期日||此会员储值卡仅能在Hamilton Nations超市门店内使用||充值折扣与店内优惠是否可以叠加使用以商户规则为准||此卡中金额不够当次消费，可扣清卡金额后用其他方式补齐（刷银行卡，付现等）",
+          "name" : "Hamilton Nations购物卡",
+          "description" : "此卡为记名制，仅限本人使用不可转借他人||此卡不可做转账或记账使用||开卡与充值一经办理，非店方原因，不兑现，不退换||此卡最终解释权归商户所有||Snaplii是第三方购物卡购买平台，此购物卡发行商为Nations超市并负责资金管理"
+        },
+        "bestDeal" : "false",
+        "cardTemplateId" : "0000004813",
+        "status" : "ACTIVE",
+        "imageUrl" : "https://snaplii.s3-us-west-2.amazonaws.com/snaplii/card/1699304835016_nations.png",
+        "discount" : "3",
+        "balanceRefreshable" : "true",
+        "reloadable" : "true",
+        "type" : "PREPAID_CARD",
+        "buyAmount" : "97",
+        "faceValueRules" : {
+          "type" : "FIXED",
+          "priceStart" : "100",
+          "priceEnd" : "100"
+        }
+      },
+      {
+        "desc" : {
+          "language" : "zh",
+          "additionalDesc" : "线上充值最高96折，可反复续充，无过期日||此会员储值卡仅能在Hamilton Nations超市门店内使用||充值折扣与店内优惠是否可以叠加使用以商户规则为准||此卡中金额不够当次消费，可扣清卡金额后用其他方式补齐（刷银行卡，付现等）",
+          "name" : "Hamilton Nations购物卡",
+          "description" : "此卡为记名制，仅限本人使用不可转借他人||此卡不可做转账或记账使用||开卡与充值一经办理，非店方原因，不兑现，不退换||此卡最终解释权归商户所有||Snaplii是第三方购物卡购买平台，此购物卡发行商为Nations超市并负责资金管理"
+        },
+        "bestDeal" : "true",
+        "cardTemplateId" : "0000004903",
+        "status" : "ACTIVE",
+        "imageUrl" : "https://snaplii.s3-us-west-2.amazonaws.com/snaplii/card/1699304835016_nations.png",
+        "discount" : "4",
+        "balanceRefreshable" : "true",
+        "reloadable" : "true",
+        "type" : "PREPAID_CARD",
+        "buyAmount" : "192",
+        "faceValueRules" : {
+          "type" : "FIXED",
+          "priceStart" : "200",
+          "priceEnd" : "200"
+        }
+      }
+    ],
+    "cardsNum" : "3",
+    "desc" : {
+      "name" : "Hamilton Nations Fresh Foods",
+      "language" : "zh",
+      "description" : "Nations Fresh Foods是一家多元文化的超市连锁店，于2012年8月28日在Woodbridge成立。2013年7月13日第二家Nations商店在Hamilton开业。虽然我们是超市和预制食品行业的新来者，但我们提供来自世界各地的各种货品，为我们的客户服务。我们有“每日新鲜，每日负担得起”的信念||商户地址：2 King St W #445, Hamilton, ON L8P 1A2 ||联系方式：9055258188"
+    },
+    "status" : "ACTIVE",
+    "merchantId" : "901951361863",
+    "tagIds1" : "",
+    "imageUrl" : "https://snaplii.s3-us-west-2.amazonaws.com/snaplii/card/1699304835016_nations%20Logo.png",
+    "tagIdsNum" : "1",
+    "cardBrandId" : "0007602"
+  },
+  "serviceTime" : "20240213062658",
+  "rspMsgInf" : "Success",
+  "environmentId" : "default"
+}
