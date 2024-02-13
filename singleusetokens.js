0fe19a2160e987046b031c10d60dbfc4e4dfeab2
Yuan <41229059+gy-0@users.noreply.github.com>
2024-02-13 01:19:53 -0500

    Create singleusetokens.js

---
+++ singleusetokens.js
@@ -0,0 +1,3 @@
+200
+
+{"id":"8000ddb3-1642-4b79-98c8-51354b011197","paymentToken":"SChSb3HeIcVo0Ltr","timeToLiveSeconds":299,"billingAddress":{"street":"","street2":"","country":"CA","zip":"L8S3Y8"},"card":{"cardBin":"519393","lastDigits":"9006","cardExpiry":{"year":2027,"month":10},"holderName":"Yuan Gao","cardType":"MC","cardCategory":"CREDIT","issuingCountry":"CA"}}
