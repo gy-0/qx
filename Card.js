d1f6d6d7780a753449befe671487787eb1991a5b
Yuan <41229059+gy-0@users.noreply.github.com>
2024-02-21 11:52:28 -0500

    Delete Card.js

--- Card.js
+++
@@ -1,1 +0,0 @@
-{"id":"18ba5a55-b5e4-4022-832f-70fc5edb68d7","paymentToken":"SCdHDYjKKxvZKXua","timeToLiveSeconds":299,"billingAddress":{"street":"","street2":"","country":"CA","zip":"L8S3Y8"},"card":{"cardBin":"123456","lastDigits":"8888","cardExpiry":{"year":2024,"month":10},"holderName":"Yuan","cardType":"MC","cardCategory":"DEBIT","issuingCountry":"CA"}}
