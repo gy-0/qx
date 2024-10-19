151689b50fc2232cf50041a88c534582c96b52d8
Yuan <41229059+gy-0@users.noreply.github.com>
2024-10-19 17:55:39 -0400

    Create BionicReading.js

---
+++ BionicReading.js
@@ -0,0 +1,33 @@
+{
+  "request_date": "2024-10-19T21:48:33Z",
+  "request_date_ms": 1729374513923,
+  "subscriber": {
+    "entitlements": {
+      "premium_plus": {
+        "expires_date": "2026-11-19T21:48:33Z",
+        "product_identifier": "bionicreading.advanced.monthly",
+        "purchase_date": "2024-10-19T21:48:33Z"
+      }
+    },
+    "first_seen": "2024-10-19T21:33:54Z",
+    "last_seen": "2024-10-19T21:48:33Z",
+    "management_url": null,
+    "non_subscriptions": {},
+    "original_app_user_id": "$RCAnonymousID:20cf719184c04bd2a8a51b85bf98d129",
+    "original_application_version": null,
+    "original_purchase_date": "2024-10-19T21:48:33Z",
+    "other_purchases": {},
+    "subscriptions": {
+      "bionicreading.advanced.monthly": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2024-11-19T21:48:33Z",
+        "is_sandbox": false,
+        "original_purchase_date": "2024-10-19T21:48:33Z",
+        "period_type": "normal",
+        "purchase_date": "2024-10-19T21:48:33Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": null
+      }
+    }
+  }
+}
