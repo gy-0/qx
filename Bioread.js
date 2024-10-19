c5f9340abbf284e0a15c4eb9742ea5daf37bcaef
Yuan <41229059+gy-0@users.noreply.github.com>
2024-10-19 18:08:46 -0400

    Create Bioread.js

---
+++ Bioread.js
@@ -0,0 +1,35 @@
+{
+  "request_date": "2024-10-19T21:48:33Z",
+  "request_date_ms": 1729374513923,
+  "subscriber": {
+    "entitlements": {
+      "revenuecat.bionic-reading.premium-plus": {
+        "expires_date": "2025-10-19T21:48:33Z",
+        "grace_period_expires_date": null,
+        "product_identifier": "bionicreading.advanced.annually",
+        "purchase_date": "2024-10-19T21:48:33Z"
+      }
+    },
+    "first_seen": "2024-10-19T21:33:54Z",
+    "last_seen": "2024-10-19T21:48:33Z",
+    "management_url": null,
+    "non_subscriptions": {},
+    "original_app_user_id": "$RCAnonymousID:20cf719184c04bd2a8a51b85bf98d129",
+    "original_application_version": null,
+    "original_purchase_date": "2024-10-19T21:48:33Z",
+    "other_purchases": {},
+    "subscriptions": {
+      "bionicreading.advanced.annually": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2025-10-19T21:48:33Z",
+        "is_sandbox": false,
+        "original_purchase_date": "2024-10-19T21:48:33Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "normal",
+        "purchase_date": "2024-10-19T21:48:33Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": null
+      }
+    }
+  }
+}
